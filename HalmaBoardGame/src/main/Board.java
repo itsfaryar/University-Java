0a39beaac9ee512a328ee2f3f53a8bbd75908974
“FaryarD” <amindaemdoost@yahoo.com>
2020-05-18 21:26:50 +0430

    modified:   HalmaBoardGame/src/main/Board.java 	modified:   HalmaBoardGame/src/main/Gui.java

--- HalmaBoardGame/src/main/Board.java
+++ HalmaBoardGame/src/main/Board.java
@@ -1,12 +1,9 @@
 package main;
 
 import java.awt.BorderLayout;
-import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JToolBar;
-import javax.swing.border.EmptyBorder;
+
 
 public class Board {
 	private final JPanel board = new JPanel(new BorderLayout(3, 3));
@@ -20,15 +17,8 @@ public class Board {
 		initBoard();
 	}
 	public void initBoard() {
-		board.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JToolBar tools = new JToolBar();
-        tools.setFloatable(false);
-        board.add(tools, BorderLayout.PAGE_START);
-        tools.add(new JButton("New")); // TODO - add functionality!
-        tools.add(new JButton("Save")); // TODO - add functionality!
-        tools.add(new JButton("Restore")); // TODO - add functionality!
-        tools.addSeparator();
-        tools.add(new JButton("Resign")); // TODO - add functionality!
+		
+      
 	}
 	public JPanel getBoard() {
 		return board;
--- HalmaBoardGame/src/main/Gui.java
+++ HalmaBoardGame/src/main/Gui.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
 
-public class Gui {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Gui implements ActionListener{
 
 	private JFrame frame;
+	private JMenuBar menu_bar;
+	private JMenu menu_new;
+	private JMenuItem new_tow,new_one,new_four;
 
 	/**
 	 * Launch the application.
@@ -37,10 +43,34 @@ public class Gui {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 640, 480);
+		
+		//contentpane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JToolBar tools = new JToolBar();
+        tools.setFloatable(false);
+        //board.add(tools, BorderLayout.PAGE_START);
+        menu_bar=new JMenuBar();
+        frame.add(menu_bar, BorderLayout.PAGE_START);
+        menu_new=new JMenu("New");
+        new_tow=new JMenuItem("Tow Players");
+        new_tow.addActionListener(this);
+        menu_bar.add(menu_new);
+        menu_new.add(new_tow);
+        //menu_new.add(new Actiona)
+        //menu_bar.add(two_pl);
+        
+        //tools.add(menu_new); 
+        tools.addSeparator();
 		Board gb=new Board();
 		frame.add(gb.getBoard());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationByPlatform(true);
 	}
+	public void actionPerformed(ActionEvent e) {    
+		if(e.getSource()==new_tow)  {
+			
+		}
+		
+		
+		}     
 
 }
